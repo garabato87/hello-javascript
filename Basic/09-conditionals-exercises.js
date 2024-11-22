4455f824f33bf1b0d8f00986b21c048e70e97b44
Braian Alvarez <alvarez.braian87@gmail.com>
2024-11-22 14:03:45 -0300

    22/11/24

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -11,18 +11,18 @@ let myName = "Braian"
 if(myName == "Braian") console.log(myName);
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
-/*let user = "usuario"
+let user = "usuario"
 let password = "password"
-let user1 = prompt("ingrese su usuario")
-let password1 = prompt("ingrese su contraseña")
-if(user1 = user & password1 == password) console.log("El usuario y contraseña son correctos");
-*/
+let user1 = "usuario"
+let password1 = "password"
+if(user1 == user & password1 == password) console.log("El usuario y contraseña son correctos");
+
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
 let number = -5
 if(number >0) {
     console.log("el numero es positivo");
     
-}else if(number = 0){
+}else if(number == 0){
     console.log("el numero es cero");
     
 }else {
@@ -33,11 +33,40 @@ console.log(number);
 
     
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+let edadBraian = 17
+let edadGuada = 16
 
+if (edadBraian >= 18) {
+    console.log("si puede votar, tiene:",edadBraian,"años");
+} else {
+    let edadFaltante = 18 - edadBraian ;
+    console.log(`No puede votar, le faltan: ${edadFaltante} años`);
+}
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
 
+let age = 20
+const message = age >= 18 ? "La persona es adulta" : "La persona es menor"
+console.log(message);
+
+
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
+let mes = "marzo"
+
+if (mes == "diciembre" || mes == "enero" || mes == "febrero") {
+    console.log("Nos encontramos en verano");
+
+} else if (mes == "marzo" || mes == "abril" || mes == "mayo") {
+    console.log("Nos encontramos en otoño");
+
+} else if (mes == "junio" || mes == "julio" || mes == "agosto") {
+    console.log("Nos encontramos en invierno");
+    
+} else {
+    console.log("Nos encontramos en primavera");
+}
+
+    
 
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
 
